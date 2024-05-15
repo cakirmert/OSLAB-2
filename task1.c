19d1866c1f6407f2fe6f18dba7ecf06fa1cf2411
Mert <66678730+cakirmert@users.noreply.github.com>
2024-05-15 12:58:47 +0200

    update

--- task1.c
+++ task1.c
@@ -1,20 +1,33 @@
 #include <stdio.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <glob.h>
+
+void truncate_files(const char *pattern) {
+    glob_t glob_result;
+    glob(pattern, 0, NULL, &glob_result);
+
+    for (size_t i = 0; i < glob_result.gl_pathc; ++i) {
+        int fd = open(glob_result.gl_pathv[i], O_WRONLY | O_TRUNC);
+        if (fd < 0) {
+            perror(glob_result.gl_pathv[i]);
+        } else {
+            close(fd);
+            printf("File %s content erased.\n", glob_result.gl_pathv[i]);
+        }
+    }
+    globfree(&glob_result);
+}
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <filename1> <filename2> ...\n", argv[0]);
         return 1;
     }
 
-    int fd = open(argv[1], O_WRONLY | O_TRUNC);
-    if (fd < 0) {
-        perror("Failed to open file");
-        return 1;
+    for (int i = 1; i < argc; i++) {
+        truncate_files(argv[i]);
     }
 
-    close(fd);
-    printf("File %s content erased.\n", argv[1]);
     return 0;
 }
--- task22.c
+++ task22.c
@@ -1,74 +1,43 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
-#include <string.h>
 
 #define DATA_FILE "datafile"
-#define MESSAGE "Hello from process "
-#define MAX_ITERATIONS 10  // Limit the number of iterations
 
-void write_to_file(const char *process_name) {
+void write_to_file(const char *message) {
     FILE *file = fopen(DATA_FILE, "w");
-    if (file == NULL) {
-        perror("Failed to open data file");
-        exit(EXIT_FAILURE);
-    }
-    fprintf(file, "%s%s\n", MESSAGE, process_name);
+    fprintf(file, "%s\n", message);
     fclose(file);
 }
 
 void read_from_file() {
     char buffer[256];
     FILE *file = fopen(DATA_FILE, "r");
-    if (file == NULL) {
-        perror("Failed to open data file");
-        exit(EXIT_FAILURE);
-    }
-    if (fgets(buffer, sizeof(buffer), file) != NULL) {
-        printf("Read from file: %s", buffer);
-    }
+    fgets(buffer, sizeof(buffer), file);
+    printf("Read from file: %s", buffer);
     fclose(file);
 }
 
-void lock_and_process(const char *process_name) {
+void lock_and_process(const char *message) {
     int fd = open(DATA_FILE, O_CREAT | O_RDWR, 0666);
-    if (fd < 0) {
-        perror("Failed to open data file");
-        exit(EXIT_FAILURE);
-    }
-
-    struct flock lock;
-    lock.l_type = F_WRLCK;
-    lock.l_whence = SEEK_SET;
-    lock.l_start = 0;
-    lock.l_len = 0;
-
+    struct flock lock = {F_WRLCK, SEEK_SET, 0, 0, 0};
     while (fcntl(fd, F_SETLK, &lock) == -1) {
-        sleep(1); // Wait if the file is locked
+        sleep(1);
     }
-
-    write_to_file(process_name);
+    write_to_file(message);
     read_from_file();
     ftruncate(fd, 0);
-
     lock.l_type = F_UNLCK;
     fcntl(fd, F_SETLK, &lock);
-
     close(fd);
 }
 
 int main(int argc, char *argv[]) {
     if (argc != 2) {
-        fprintf(stderr, "Usage: %s <process_name>\n", argv[0]);
+        fprintf(stderr, "Usage: %s <message>\n", argv[0]);
         return 1;
     }
 
-    for (int i = 0; i < MAX_ITERATIONS; i++) {
-        lock_and_process(argv[1]);
-        sleep(2); // Simulate some delay
-    }
-
-    printf("Process %s completed.\n", argv[1]);
+    lock_and_process(argv[1]);
     return 0;
 }
